02596c2af33486ee48678ae2c58833f4a1e22fcc
donovanroubos <donovan@roubos.co>
2025-10-21 15:25:20 +0200

    feat: Add Inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,19 @@
 import typography from '@tailwindcss/typography'
-
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif']
+      }
+    }
+  },
   plugins: [typography]
 }
 
