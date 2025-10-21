8a73feb5946587b151a0b7df358d94324527297d
Teun Kelting <teun@yummygum.com>
2025-10-21 15:53:55 +0200

    fix: correctly apply eslint configs
    # Conflicts:
    #	src/app/layout.tsx
    #	tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <Image
-          className="dark:invert"
-          src="/built-with-iota.svg"
           alt="Next.js logo"
-          width={180}
+          className="dark:invert"
           height={38}
           priority
+          src="/built-with-iota.svg"
+          width={180}
         />
         <h1>IOTA Gas Station</h1>
       </main>
     </div>
   )
 }
+
+export default Home
