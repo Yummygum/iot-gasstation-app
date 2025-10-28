9526495520be712f9be45a173932900d4ee1886d
Teun Kelting <teun@yummygum.com>
2025-10-28 16:43:34 +0100

    chore: tweak chart

--- src/components/Chart/ExampleChart.tsx
+++ src/components/Chart/ExampleChart.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts'
 
 import {
@@ -28,152 +28,152 @@ import {
   ItemTitle
 } from '../ui/item'
 
-export const description = 'An interactive area chart'
-
 const chartData = [
-  { date: '2024-04-01', gas: 222 },
-  { date: '2024-04-02', gas: 97 },
-  { date: '2024-04-03', gas: 167 },
-  { date: '2024-04-04', gas: 242 },
-  { date: '2024-04-05', gas: 373 },
-  { date: '2024-04-06', gas: 301 },
-  { date: '2024-04-07', gas: 245 },
-  { date: '2024-04-08', gas: 409 },
-  { date: '2024-04-09', gas: 59 },
-  { date: '2024-04-10', gas: 261 },
-  { date: '2024-04-11', gas: 327 },
-  { date: '2024-04-12', gas: 292 },
-  { date: '2024-04-13', gas: 342 },
-  { date: '2024-04-14', gas: 137 },
-  { date: '2024-04-15', gas: 120 },
-  { date: '2024-04-16', gas: 138 },
-  { date: '2024-04-17', gas: 446 },
-  { date: '2024-04-18', gas: 364 },
-  { date: '2024-04-19', gas: 243 },
-  { date: '2024-04-20', gas: 89 },
-  { date: '2024-04-21', gas: 137 },
-  { date: '2024-04-22', gas: 224 },
-  { date: '2024-04-23', gas: 138 },
-  { date: '2024-04-24', gas: 387 },
-  { date: '2024-04-25', gas: 215 },
-  { date: '2024-04-26', gas: 75 },
-  { date: '2024-04-27', gas: 383 },
-  { date: '2024-04-28', gas: 122 },
-  { date: '2024-04-29', gas: 315 },
-  { date: '2024-04-30', gas: 454 },
-  { date: '2024-05-01', gas: 165 },
-  { date: '2024-05-02', gas: 293 },
-  { date: '2024-05-03', gas: 247 },
-  { date: '2024-05-04', gas: 385 },
-  { date: '2024-05-05', gas: 481 },
-  { date: '2024-05-06', gas: 498 },
-  { date: '2024-05-07', gas: 388 },
-  { date: '2024-05-08', gas: 149 },
-  { date: '2024-05-09', gas: 227 },
-  { date: '2024-05-10', gas: 293 },
-  { date: '2024-05-11', gas: 335 },
-  { date: '2024-05-12', gas: 197 },
-  { date: '2024-05-13', gas: 197 },
-  { date: '2024-05-14', gas: 448 },
-  { date: '2024-05-15', gas: 473 },
-  { date: '2024-05-16', gas: 338 },
-  { date: '2024-05-17', gas: 499 },
-  { date: '2024-05-18', gas: 315 },
-  { date: '2024-05-19', gas: 235 },
-  { date: '2024-05-20', gas: 177 },
-  { date: '2024-05-21', gas: 82 },
-  { date: '2024-05-22', gas: 81 },
-  { date: '2024-05-23', gas: 252 },
-  { date: '2024-05-24', gas: 294 },
-  { date: '2024-05-25', gas: 201 },
-  { date: '2024-05-26', gas: 213 },
-  { date: '2024-05-27', gas: 420 },
-  { date: '2024-05-28', gas: 233 },
-  { date: '2024-05-29', gas: 78 },
-  { date: '2024-05-30', gas: 340 },
-  { date: '2024-05-31', gas: 178 },
-  { date: '2024-06-01', gas: 178 },
-  { date: '2024-06-02', gas: 470 },
-  { date: '2024-06-03', gas: 103 },
-  { date: '2024-06-04', gas: 439 },
-  { date: '2024-06-05', gas: 88 },
-  { date: '2024-06-06', gas: 294 },
-  { date: '2024-06-07', gas: 323 },
-  { date: '2024-06-08', gas: 385 },
-  { date: '2024-06-09', gas: 438 },
-  { date: '2024-06-10', gas: 155 },
-  { date: '2024-06-11', gas: 92 },
-  { date: '2024-06-12', gas: 492 },
-  { date: '2024-06-13', gas: 81 },
-  { date: '2024-06-14', gas: 426 },
-  { date: '2024-06-15', gas: 307 },
-  { date: '2024-06-16', gas: 371 },
-  { date: '2024-06-17', gas: 475 },
-  { date: '2024-06-18', gas: 107 },
-  { date: '2024-06-19', gas: 341 },
-  { date: '2024-06-20', gas: 408 },
-  { date: '2024-06-21', gas: 169 },
-  { date: '2024-06-22', gas: 317 },
-  { date: '2024-06-23', gas: 480 },
-  { date: '2024-06-24', gas: 132 },
-  { date: '2024-06-25', gas: 141 },
-  { date: '2024-06-26', gas: 434 },
-  { date: '2024-06-27', gas: 448 },
-  { date: '2024-06-28', gas: 149 },
-  { date: '2024-06-29', gas: 103 },
-  { date: '2024-06-30', gas: 446 }
+  { date: '2024-04-01', gas: 22 },
+  { date: '2024-04-02', gas: 9 },
+  { date: '2024-04-03', gas: 16 },
+  { date: '2024-04-04', gas: 24 },
+  { date: '2024-04-05', gas: 37 },
+  { date: '2024-04-06', gas: 30 },
+  { date: '2024-04-07', gas: 24 },
+  { date: '2024-04-08', gas: 40 },
+  { date: '2024-04-09', gas: 5 },
+  { date: '2024-04-10', gas: 26 },
+  { date: '2024-04-11', gas: 32 },
+  { date: '2024-04-12', gas: 29 },
+  { date: '2024-04-13', gas: 34 },
+  { date: '2024-04-14', gas: 13 },
+  { date: '2024-04-15', gas: 12 },
+  { date: '2024-04-16', gas: 13 },
+  { date: '2024-04-17', gas: 44 },
+  { date: '2024-04-18', gas: 36 },
+  { date: '2024-04-19', gas: 24 },
+  { date: '2024-04-20', gas: 8 },
+  { date: '2024-04-21', gas: 13 },
+  { date: '2024-04-22', gas: 22 },
+  { date: '2024-04-23', gas: 13 },
+  { date: '2024-04-24', gas: 38 },
+  { date: '2024-04-25', gas: 21 },
+  { date: '2024-04-26', gas: 7 },
+  { date: '2024-04-27', gas: 38 },
+  { date: '2024-04-28', gas: 12 },
+  { date: '2024-04-29', gas: 31 },
+  { date: '2024-04-30', gas: 45 },
+  { date: '2024-05-01', gas: 16 },
+  { date: '2024-05-02', gas: 29 },
+  { date: '2024-05-03', gas: 24 },
+  { date: '2024-05-04', gas: 38 },
+  { date: '2024-05-05', gas: 48 },
+  { date: '2024-05-06', gas: 49 },
+  { date: '2024-05-07', gas: 38 },
+  { date: '2024-05-08', gas: 14 },
+  { date: '2024-05-09', gas: 22 },
+  { date: '2024-05-10', gas: 29 },
+  { date: '2024-05-11', gas: 33 },
+  { date: '2024-05-12', gas: 19 },
+  { date: '2024-05-13', gas: 19 },
+  { date: '2024-05-14', gas: 44 },
+  { date: '2024-05-15', gas: 47 },
+  { date: '2024-05-16', gas: 33 },
+  { date: '2024-05-17', gas: 49 },
+  { date: '2024-05-18', gas: 31 },
+  { date: '2024-05-19', gas: 23 },
+  { date: '2024-05-20', gas: 17 },
+  { date: '2024-05-21', gas: 8 },
+  { date: '2024-05-22', gas: 8 },
+  { date: '2024-05-23', gas: 25 },
+  { date: '2024-05-24', gas: 29 },
+  { date: '2024-05-25', gas: 20 },
+  { date: '2024-05-26', gas: 21 },
+  { date: '2024-05-27', gas: 42 },
+  { date: '2024-05-28', gas: 23 },
+  { date: '2024-05-29', gas: 7 },
+  { date: '2024-05-30', gas: 34 },
+  { date: '2024-05-31', gas: 17 },
+  { date: '2024-06-01', gas: 17 },
+  { date: '2024-06-02', gas: 47 },
+  { date: '2024-06-03', gas: 10 },
+  { date: '2024-06-04', gas: 43 },
+  { date: '2024-06-05', gas: 8 },
+  { date: '2024-06-06', gas: 29 },
+  { date: '2024-06-07', gas: 32 },
+  { date: '2024-06-08', gas: 38 },
+  { date: '2024-06-09', gas: 43 },
+  { date: '2024-06-10', gas: 15 },
+  { date: '2024-06-11', gas: 9 },
+  { date: '2024-06-12', gas: 49 },
+  { date: '2024-06-13', gas: 8 },
+  { date: '2024-06-14', gas: 42 },
+  { date: '2024-06-15', gas: 30 },
+  { date: '2024-06-16', gas: 37 },
+  { date: '2024-06-17', gas: 47 },
+  { date: '2024-06-18', gas: 10 },
+  { date: '2024-06-19', gas: 34 },
+  { date: '2024-06-20', gas: 40 },
+  { date: '2024-06-21', gas: 16 },
+  { date: '2024-06-22', gas: 31 },
+  { date: '2024-06-23', gas: 48 },
+  { date: '2024-06-24', gas: 13 },
+  { date: '2024-06-25', gas: 14 },
+  { date: '2024-06-26', gas: 43 },
+  { date: '2024-06-27', gas: 26 },
+  { date: '2024-06-28', gas: 14 },
+  { date: '2024-06-29', gas: 10 },
+  { date: '2024-06-30', gas: 18 }
 ]
 
 const chartConfig = {
   gas: {
     label: 'Gas spent',
-    color: 'var(--chart-2)',
+    color: 'var(--chart-1)',
     icon: IOTASymbol
   }
 } satisfies ChartConfig
 
 const ExampleChart = () => {
-  const [timeRange, setTimeRange] = useState('90d')
+  const [timeRangeDays, setTimeRangeDays] = useState(7)
 
   const filteredData = chartData.filter((item) => {
     const date = new Date(item.date)
     const referenceDate = new Date('2024-06-30')
-    let daysToSubtract = 90
-    if (timeRange === '30d') {
-      daysToSubtract = 30
-    } else if (timeRange === '7d') {
-      daysToSubtract = 7
-    }
+    const daysToSubtract = timeRangeDays
 
     const startDate = new Date(referenceDate)
     startDate.setDate(startDate.getDate() - daysToSubtract)
     return date >= startDate
   })
 
+  const description = useMemo(() => {
+    return `Showing gas spent for the last ${timeRangeDays} days`
+  }, [timeRangeDays])
+
   return (
     <Item className="pt-0" variant="outline">
       <ItemHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1">
           <ItemTitle>Total gas spent</ItemTitle>
-          <ItemDescription>
-            Showing gas spent for the last 3 months
-          </ItemDescription>
+          <ItemDescription>{description}</ItemDescription>
         </div>
-        <Select onValueChange={setTimeRange} value={timeRange}>
+        <Select
+          onValueChange={(selectedValue) =>
+            setTimeRangeDays(parseInt(selectedValue, 10))
+          }
+          value={String(timeRangeDays)}
+        >
           <SelectTrigger
             aria-label="Select a value"
             className="hidden w-[160px] rounded-lg sm:ml-auto sm:flex"
           >
-            <SelectValue placeholder="Last 3 months" />
+            <SelectValue placeholder="Last 90 days" />
           </SelectTrigger>
           <SelectContent className="rounded-xl">
-            <SelectItem className="rounded-lg" value="90d">
+            <SelectItem className="rounded-lg" value="90">
               Last 3 months
             </SelectItem>
-            <SelectItem className="rounded-lg" value="30d">
+            <SelectItem className="rounded-lg" value="30">
               Last 30 days
             </SelectItem>
-            <SelectItem className="rounded-lg" value="7d">
+            <SelectItem className="rounded-lg" value="7">
               Last 7 days
             </SelectItem>
           </SelectContent>
