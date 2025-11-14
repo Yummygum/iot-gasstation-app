427d49bd8925d32a95dd187f0b3221a0040b88c7
Teun Kelting <teun@yummygum.com>
2025-11-14 16:42:07 +0100

    feat(SettingsDialog): add initial version of component

--- src/lib/api/queries/getSponsorWallet.ts
+++ src/lib/api/queries/getSponsorWallet.ts
@@ -1,4 +1,4 @@
-import { graphql } from '../graphql'
+import { graphql, ResultOf } from '../graphql'
 
 const GET_SPONSOR_WALLET = graphql(`
   query GetSponsorWallet {
@@ -18,5 +18,7 @@ const GET_SPONSOR_WALLET = graphql(`
     }
   }
 `)
+
+export type GetSponsorWalletQuery = ResultOf<typeof GET_SPONSOR_WALLET>
 
 export default GET_SPONSOR_WALLET
